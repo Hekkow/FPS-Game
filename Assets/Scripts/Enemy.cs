72534dfd5948ebd67b54068d5a8f021f1caa68e1
Hekkow <43420295+Hekkow@users.noreply.github.com>
2023-01-13 22:57:52 -0500

    finally done adding jumps god bless america

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -30,6 +30,14 @@ public class Enemy : MonoBehaviour
     [SerializeField] float knockedTime;
     [SerializeField] float ragdollMass;
 
+    [Header("Pathfinding")]
+    [SerializeField] float distanceHeightMultiplier;
+    [SerializeField] float tinyBitOfExtraHeight;
+    [SerializeField] float enemyHeight;
+    [SerializeField] float groundHeight;
+    [SerializeField] float timeBeforeGroundCheck;
+    [SerializeField] float extraDownwardsDistance;
+
     [HideInInspector] public bool knocked = false;
 
     GameObject instantiatedAnimation;
@@ -43,14 +51,13 @@ public class Enemy : MonoBehaviour
     float viewRadius;
     float viewAngle;
     float timeDetected;
-
+    float gravity = -Physics.gravity.y;
     bool playerDetected = false;
     bool detectedOnce = false;
 
     bool alreadyAttacked = false;
 
     bool jumping = false;
-    bool once = false;
 
     IEnumerator knockCoroutine;
 
@@ -69,23 +76,19 @@ public class Enemy : MonoBehaviour
 
         
     }
-
     void Update()
     {
         if (!agent.isOnOffMeshLink && !jumping)
         {
             agent.SetDestination(player.transform.position);
-            //transform.LookAt(player.transform.position);
 
         }
         else
         {
             if (!jumping)
             {
-                Jump();
+                StartCoroutine(Jump());
             }
-            
-            //agent.CompleteOffMeshLink();
         }
         //if (!health.alive)
         //{
@@ -102,52 +105,65 @@ public class Enemy : MonoBehaviour
         //}
         UpdateHealthBar();
     }
-    void Jump()
-    {
-        //Vector3 startPos = agent.transform.position;
-        Vector3 endPos = agent.currentOffMeshLinkData.endPos;
-        StartCoroutine(WaitUntilLanded());
-        //agent.transform.position = endPos;
-        //transform.position = endPos;
-        
-        
-    }
-    IEnumerator WaitUntilLanded()
+    IEnumerator Jump()
     {
         Vector3 startPos = agent.currentOffMeshLinkData.startPos;
         Vector3 endPos = agent.currentOffMeshLinkData.endPos;
-        bool down = startPos.y > endPos.y;
-        transform.position = new Vector3(startPos.x, transform.position.y, startPos.z);
+        // controlled by rigidbody now
+        jumping = true;
+        rb.isKinematic = false;
+        agent.enabled = false;
+
+        // move towards starting location
+        Vector3 velocityToStart = (startPos - transform.position).normalized * agent.speed;
+        float distanceToStart = Mathf.Sqrt(Mathf.Pow(startPos.x - transform.position.x, 2) + Mathf.Pow(startPos.z - transform.position.z, 2));
+        float timeToStart = distanceToStart / agent.speed;
+        rb.velocity = new Vector3(velocityToStart.x, 0, velocityToStart.z);
+        yield return new WaitForSeconds(timeToStart);
+        rb.velocity = Vector3.zero;
+
+
+        float actualTime = Time.time;
         float distance = Mathf.Sqrt(Mathf.Pow(startPos.x - endPos.x, 2) + Mathf.Pow(startPos.z - endPos.z, 2));
-        float extraHeight = 0.083333f + distance/10;
+        float extraHeight = tinyBitOfExtraHeight + distance/distanceHeightMultiplier;
         float height = endPos.y - startPos.y + extraHeight;
         float angle = Mathf.Rad2Deg * Mathf.Atan(height / distance);
         float timeUp;
         float timeDown;
         if (angle < 0)
         {
-            timeUp = Mathf.Sqrt((2 * -height) / -Physics.gravity.y);
-            timeDown = Mathf.Sqrt((2 * extraHeight) / -Physics.gravity.y);
+            timeUp = Mathf.Sqrt((2 * extraHeight) / gravity);
+            timeDown = Mathf.Sqrt(Mathf.Abs((2 * height) / gravity));
         }
         else
         {
-            timeUp = Mathf.Sqrt((2 * height) / -Physics.gravity.y);
-            timeDown = Mathf.Sqrt((2 * extraHeight) / -Physics.gravity.y);
+            timeUp = Mathf.Sqrt((2 * height) / gravity);
+            timeDown = Mathf.Sqrt((2 * extraHeight) / gravity);
         }
-        float time = timeUp + timeDown;
-        jumping = true;
-        rb.isKinematic = false;
-        agent.enabled = false;
-        float horizontalVelocity = distance;
-        float velocityY = (height / time) + (-Physics.gravity.y * time/2);
+        float jumpTime = timeUp + timeDown;
+        float velocityY = (height / jumpTime) + (gravity * jumpTime/2);
         Vector3 velocity = (endPos-startPos).normalized;
-        rb.velocity = new Vector3((velocity.x * distance) / timeUp, velocityY, (velocity.z * distance) / timeUp);
-        yield return new WaitForSeconds(0.1f);
-        yield return new WaitUntil(() => Physics.Raycast(transform.position, -Vector3.up, 1 + 0.1f));
+
+        // if going down, keep moving until off platform
+        if (angle < 0)
+        {
+            while (Physics.Raycast(transform.position, -Vector3.up, enemyHeight + groundHeight))
+            {
+                rb.velocity = new Vector3((velocity.x * (distance+extraDownwardsDistance)) / timeDown, 0, (velocity.z * (distance+ extraDownwardsDistance)) / timeDown);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector3((velocity.x * distance) / timeUp, velocityY, (velocity.z * distance) / timeUp);
+        }
+        // wait until landed
+        yield return new WaitForSeconds(timeBeforeGroundCheck);
+        yield return new WaitUntil(() => Physics.Raycast(transform.position, -Vector3.up, enemyHeight + groundHeight));
+        // reset
         jumping = false;
         rb.isKinematic = true;
         agent.enabled = true;
-        agent.isStopped = false;
         agent.CompleteOffMeshLink();
     }
     void Vision()
